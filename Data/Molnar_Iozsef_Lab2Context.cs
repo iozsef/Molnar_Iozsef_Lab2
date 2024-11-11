35857add209720f28b74dae3e65c91b409844fd6
yogibear <yogibear@KRISTOF>
2024-11-11 23:00:48 +0200

    Add project files.

---
+++ Data/Molnar_Iozsef_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Molnar_Iozsef_Lab2.Models;
+
+namespace Molnar_Iozsef_Lab2.Data
+{
+    public class Molnar_Iozsef_Lab2Context : DbContext
+    {
+        public Molnar_Iozsef_Lab2Context (DbContextOptions<Molnar_Iozsef_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Molnar_Iozsef_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Molnar_Iozsef_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+        public DbSet<Molnar_Iozsef_Lab2.Models.Author> Author { get; set; } = default!;
+    }
+}
---
+++ Models/Author.cs
@@ -0,0 +1,12 @@
+﻿namespace Molnar_Iozsef_Lab2.Models
+{
+    public class Author
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public ICollection<Book>? Books { get; set; } //Navigation property
+
+    }
+}
